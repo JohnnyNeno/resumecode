c9e0c43b72469edbb205cb7d2377a7c9b4538b38
JohnnyNeno <37516310+JohnnyNeno@users.noreply.github.com>
2018-03-18 22:33:16 +0200

    Delete Exportable.java

--- Exportable.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.tree.DefaultMutableTreeNode;
-import java.util.ArrayList;
-import java.util.List;
-
-public interface Exportable {
-	
-	abstract void exporting(List<DefaultMutableTreeNode> tnl);
-
-}
